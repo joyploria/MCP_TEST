8c4566603209f0f9486d8393c669a61570e78174
Dev User <dev@example.com>
2025-09-04 23:35:38 +0900

    refactor: sanitize quotes consistently

--- src/sanitizer.py
+++ src/sanitizer.py
@@ -1,4 +1,5 @@
 def sanitize(s):
-    return s.replace(''', '`')
+    s = s.replace('"', '`')
+    return s.replace("'", '`')
 
-# touch: 2025-09-04T14:34:52.436907Z
+# touch: 2025-09-04T14:35:38.414173Z
