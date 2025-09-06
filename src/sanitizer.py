5d9bfff78d0d7a2b92f9eecdea39d0ee10c071a7
Dev User <dev@example.com>
2025-09-07 04:41:43 +0900

    fix(security): sanitize input

--- src/sanitizer.py
+++ src/sanitizer.py
@@ -1,5 +1,4 @@
 def sanitize(s):
-    s = s.replace('"', '`')
-    return s.replace("'", '`')
+    return s.replace('\\', '')
 
-# touch: 2025-09-06T19:34:58.382282Z
+# touch: 2025-09-06T19:41:43.753106Z
